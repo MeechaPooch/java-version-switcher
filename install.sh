a23ccb275bef72f61552c29dad8c56460d362de5
MeechaPooch <powchm@catlin.edu>
2022-11-21 23:40:53 -0800

    add proper .profile file refs

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ mkdir -p ~/.local/bin
 curl "https://raw.githubusercontent.com/MeechaPooch/java-version-switcher/main/switch" >> ~/.local/bin/switch
 chmod +x ~/.local/bin/switch
 # add switch bin to path in user zshrc and profile
-echo "export PATH=~/.local/bin:$PATH" >> .zshrc
-echo "export PATH=~/.local/bin:$PATH" >> .profile
-echo "export PATH=~/.local/bin:$PATH" >> .bash_profile
+echo "export PATH=~/.local/bin:$PATH" >> ~/.zshrc
+echo "export PATH=~/.local/bin:$PATH" >> ~/.profile
+echo "export PATH=~/.local/bin:$PATH" >> ~/.bash_profile
 # add alias so that version switches apply immediately without requiring a shell restart
-echo "alias switch=\"switch;source ~/.setjava.sh\"" >> .zshrc
-echo "alias switch=\"switch;source ~/.setjava.sh\"" >> .profile
-echo "alias switch=\"switch;source ~/.setjava.sh\"" >> .bash_profile
+echo "alias switch=\"switch;source ~/.setjava.sh\"" >> ~/.zshrc
+echo "alias switch=\"switch;source ~/.setjava.sh\"" >> ~/.profile
+echo "alias switch=\"switch;source ~/.setjava.sh\"" >> ~/.bash_profile
 
 echo "INSTALLATION SUCCESS!"
 
