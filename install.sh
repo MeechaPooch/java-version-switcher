ca4076fb852202d09e504ea9cd78e8eaf8c7f168
MeechaPooch <powchm@catlin.edu>
2022-11-22 12:18:18 -0800

    reference path variable in string

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ mkdir -p ~/.local/bin
 curl "https://raw.githubusercontent.com/MeechaPooch/java-version-switcher/main/switch" > ~/.local/bin/switch
 chmod +x ~/.local/bin/switch
 # add switch bin to path in user zshrc and profile
-echo "export PATH=~/.local/bin:$PATH" >> ~/.zshrc
-echo "export PATH=~/.local/bin:$PATH" >> ~/.profile
-echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
+echo "export PATH=~/.local/bin:\$PATH" >> ~/.zshrc
+echo "export PATH=~/.local/bin:\$PATH" >> ~/.profile
+echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
 # add alias so that version switches apply immediately without requiring a shell restart
 echo "alias switch=\"switch;source ~/.setjava.sh\"" >> ~/.zshrc
 echo "alias switch=\"switch;source ~/.setjava.sh\"" >> ~/.profile
