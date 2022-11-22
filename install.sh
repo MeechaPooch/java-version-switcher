fc4e86b64a0b2c78cbcee196d74f4791fafc4c20
MeechaPooch <powchm@catlin.edu>
2022-11-22 12:16:05 -0800

    dont append to switch, overwrite it instead

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # download switch bin to ~/.local/bin
 mkdir -p ~/.local/bin
-curl "https://raw.githubusercontent.com/MeechaPooch/java-version-switcher/main/switch" >> ~/.local/bin/switch
+curl "https://raw.githubusercontent.com/MeechaPooch/java-version-switcher/main/switch" > ~/.local/bin/switch
 chmod +x ~/.local/bin/switch
 # add switch bin to path in user zshrc and profile
 echo "export PATH=~/.local/bin:$PATH" >> ~/.zshrc
